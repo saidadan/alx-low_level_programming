df22353719d41a4c2856fdb50f218e1a1391a7a6
saidadan <saidadan05@gmail.com>
2023-10-05 07:04:09 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is the entry point of the function
+ *
+ * Return: returns value of 0 when successful
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
