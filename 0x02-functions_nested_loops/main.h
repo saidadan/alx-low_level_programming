d0f7ceb97caddcdd9a3123156b446a7392e2f241
saidadan <saidadan05@gmail.com>
2023-10-11 23:36:28 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
-int _putchar(char c);
-void print_alphabet(void);
 
-#endif
+#include <unistd.h>  /* Include this for the write function */
+
+int _putchar(char c) __attribute__((unused));
+void print_alphabet_x10(void);
+
+#endif /* MAIN_H */
+
