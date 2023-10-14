d3daa8219e784ac73f075f5a168a4de2e3f6e306
saidadan <saidadan05@gmail.com>
2023-10-14 11:17:00 -0700

    0-main

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,70 +1,52 @@
 #include <stdio.h>
 
+
 /**
- * print_fibonacci_series - Prints the first 90 Fibonacci numbers
+ * main _ The main function
+ * 1 and 2, seperated by a comma
+ * Return: 0.
  */
-void print_fibonacci_series(void)
+
+
+int main(void)
+
+
 {
 	int count;
-	unsigned long fib1 = 1, fib2 = 2, sum;
-
-	for (count = 1; count <= 90; count++)
-	{
-		printf("%lu", fib1);
-		if (count < 90)
-			printf(", ");
-		sum = fib1 + fib2;
-		fib1 = fib2;
-		fib2 = sum;
-	}
-
-	printf("\n");
-}
-
-/**
- * print_large_fibonacci_numbers - Prints the remaining large Fibonacci numbers
- */
-void print_large_fibonacci_numbers(void)
-{
-	unsigned long fib1_half1 = 0, fib1_half2 = 1;
-	unsigned long fib2_half1 = 0, fib2_half2 = 2;
+	unsigned long fib1 = 0, fib2 = 1, sum;
+	unsigned long fib1_half1, fib2_half1, fib2_half2;
 	unsigned long half1, half2;
 
-	for (int count = 91; count < 98; count++)
+
+	for (count = 0; count < 92; count++)
 	{
-		half1 = fib1_half1 + fib2_half1;
-		half2 = fib1_half2 + fib2_half2;
-
-		if (half2 > 9999999999)
-		{
-			half1 += 1;
-			half2 %= 10000000000;
-		}
-
-		printf("%lu%010lu", half1, half2);
-		if (count < 97)
-			printf(", ");
-
-		fib1_half1 = fib2_half1;
-		fib1_half2 = fib2_half2;
-		fib2_half1 = half1;
-		fib2_half2 = half2;
+	sum = fib1 + fib2;
+	printf("%lu, ", sum);
+	fib1 = fib2;
+	fib2 = sum;
 	}
-
+	fib1_half1 = fib1 / 10000000000;
+	fib2_half1 = fib2 / 10000000000;
+	fib1_half2 = fib1 % 10000000000;
+	fib2_half2 = fib2 % 10000000000;
+	for (count = 93; count < 99; count++)
+	{
+	half1 = fib1_half1 + fib2_half1;
+	half2 = fib1_half2 + fib2_half2;
+	if (fib1_half2 + fib2_half2 > 9999999999)
+	{
+	half1 += 1;
+	half2 %= 10000000000;
+	}
+	printf("%lu%lu", half1, half2);
+	if (count != 98)
+	printf(", ");
+	fib1_half1 = fib2_half1;
+	fib1_half2 = fib2_half2;
+	fib2_half1 = half1;
+	fib2_half2 = half2;
+	}
 	printf("\n");
-}
-
-/**
- * main - Entry point
- *
- * Description: This program calculates and prints the first 98 Fibonacci
- * numbers, where each number is separated by a comma and a space.
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	print_fibonacci_series();
-	print_large_fibonacci_numbers();
 	return (0);
 }
+
