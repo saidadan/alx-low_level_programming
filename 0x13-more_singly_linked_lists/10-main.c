eb15525cc314a9eb49efc985f7dbe8ede992cbb3
saidadan <saidadan05@gmail.com>
2024-08-05 11:48:06 -0400

    0x13. C - More singly linked lists

---
+++ 0x13-more_singly_linked_lists/10-main.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	listint_t *head;
+
+	head = NULL;
+	add_nodeint_end(&head, 0);
+	add_nodeint_end(&head, 1);
+	add_nodeint_end(&head, 2);
+	add_nodeint_end(&head, 3);
+	add_nodeint_end(&head, 4);
+	add_nodeint_end(&head, 98);
+	add_nodeint_end(&head, 402);
+	add_nodeint_end(&head, 1024);
+
+	print_listint(head);
+	printf("-----------------\n");
+
+	for (int i = 0; i < 12; i++)
+	{
+		delete_nodeint_at_index(&head, 5);
+		print_listint(head);
+		printf("-----------------\n");
+	}
+
+	return (0);
+}
