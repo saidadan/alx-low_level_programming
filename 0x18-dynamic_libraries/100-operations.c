b765cf59bef49b2448168d4726eb5e1cf63ca800
saidadan <saidadan05@gmail.com>
2024-09-25 13:14:54 -0400

    Task 2

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+
+/**
+ * add - Adds two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The sum of a and b.
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - Subtracts the second integer from the first.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The difference of a and b.
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - Multiplies two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The product of a and b.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - Divides the first integer by the second.
+ * @a: The numerator.
+ * @b: The denominator.
+ *
+ * Return: The quotient of a divided by b.
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - Computes the modulus of the first integer by the second.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The remainder of a divided by b.
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
+
